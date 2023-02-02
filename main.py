c0a97165d85f3b1dc7a8462a4f6c6fd0288cb98a
Chris Bertani <>
2023-02-02 15:35:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# formatted strings
+
+name = 'Johnny'
+age = 55
+
+print(f'hi {name}. You are {age} years old.')
+weather = "\t it\'s \"kind of\" sunny \n hope you have a good day"
+print(weather)
+selfish = 'me me me'
+#   01234567
+print(selfish[::-1])
+
+# built in functions/methods
+quote = 'to be or not to be'
+print(quote.replace('be', 'trap'))
+print(quote)
+quote2 = quote.replace('be', 'me')
+print(quote2)
+print(quote2.upper())
